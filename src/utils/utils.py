0a280fb4d7be76599222033af095f90bd6a8c1f9
AmazingDD <1172392977@qq.com>
2019-08-08 17:03:20 +0800

    add optimizer selector

---
+++ src/utils/utils.py
@@ -0,0 +1,18 @@
+import torch
+
+def use_optimizer(network, params):
+    if params['optimizer'] == 'sgd':
+        optimizer = torch.optim.SGD(network.parameters(),
+                                    lr=params['sgd_lr'],
+                                    momentum=params['sgd_momentum'],
+                                    weight_decay=params['l2_reg'])
+    elif params['optimizer'] == 'adam':
+        optimizer = torch.optim.Adam(network.parameters(), 
+                                                          lr=params['adam_lr'],
+                                                          weight_decay=params['l2_reg'])
+    elif params['optimizer'] == 'rmsprop':
+        optimizer = torch.optim.RMSprop(network.parameters(),
+                                        lr=params['rmsprop_lr'],
+                                        alpha=params['rmsprop_alpha'],
+                                        momentum=params['rmsprop_momentum'])
+    return optimizer
