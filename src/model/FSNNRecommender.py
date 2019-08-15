3532b105845aa2ee52480ea772317af893922248
AmazingDD <1172392977@qq.com>
2019-08-15 13:56:33 +0800

    change file name

--- src/model/FSNNRecommender.py
+++ src/model/FSNNRecommender.py
@@ -2,7 +2,7 @@
 @Author: Yu Di
 @Date: 2019-08-15 13:32:58
 @LastEditors: Yudi
-@LastEditTime: 2019-08-15 13:47:12
+@LastEditTime: 2019-08-15 13:56:22
 @Company: Cardinal Operation
 @Email: yudi@shanshu.ai
 @Description: 
@@ -10,7 +10,7 @@
 import torch
 from utils.fm_layers import FeaturesEmbedding, MultiLayerPerceptron
 
-class FSNN(torch.nn.Module):
+class FsNN(torch.nn.Module):
     def __init__(self, params):
         super().__init__()
         self.embedding = FeaturesEmbedding(params['field_dims'], params['embed_dim'])
